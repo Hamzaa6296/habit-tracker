1205342aeda1fac7ef4a230252f730511bfee640
Ali Hamza <ali.hamzaaa6296@gmail.com>
2026-07-16 17:04:53 +0500

    feature: added some configurations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Post()
-  getHello2(): string {
     console.log(this.configService.get<string>('database.uri'));
     return 'Hello world';
   }
