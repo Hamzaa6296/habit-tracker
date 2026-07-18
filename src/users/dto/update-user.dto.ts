f5ed10d12af48bc4b5ec622607bb42b247688ebd
Ali Hamza <ali.hamzaaa6296@gmail.com>
2026-07-18 23:00:14 +0500

    working on schemas

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,23 @@
+import {
+  IsString,
+  MinLength,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+
 export class UpdateUserDto {
-  name?: string;
-  email?: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  name!: string;
+
+  @IsEmail()
+  @IsOptional()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @IsOptional()
   password?: string;
 }
