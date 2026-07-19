344fd9bf41a7c916633acdc9159b4fcfbef09a27
Ali Hamza <ali.hamzaaa6296@gmail.com>
2026-07-19 21:21:42 +0500

    feature: added auth module and controller files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,28 @@
-// import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
-// import { UsersModule } from '../users/users.module';
+import { UsersModule } from '../users/users.module';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('jwt.secret'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
